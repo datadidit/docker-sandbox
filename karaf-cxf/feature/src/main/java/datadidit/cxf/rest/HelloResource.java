6edc9269ea2dde89a40aa927750659ce6805efc9
Marcus <mkwyche@gmail.com>
2017-05-21 04:24:21 -0400

    Updates to add example REST resource

--- karaf-cxf/feature/src/main/java/datadidit/cxf/rest/HelloResource.java
+++ karaf-cxf/feature/src/main/java/datadidit/cxf/rest/HelloResource.java
@@ -1,5 +1,25 @@
 package datadidit.cxf.rest;
 
-public class HelloResource {
+import javax.ws.rs.DefaultValue;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
+@Path("/")
+public class HelloResource {
+	@GET
+	@Produces({MediaType.TEXT_PLAIN, MediaType.APPLICATION_JSON})
+	public Response sayHello(){
+		return Response.ok("Hello World").build();
+	}
+	
+	@GET
+	@Produces({MediaType.TEXT_PLAIN, MediaType.APPLICATION_JSON})
+	@Path("{name}")
+	public Response sayHelloToSomeone(@PathParam("name") @DefaultValue("world") String name){
+		return Response.ok("Hello "+name).build();
+	}
 }
